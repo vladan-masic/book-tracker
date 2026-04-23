45b69645c9ee896130f8220dd77627ecbeea5a28
Vladan Masic <vladanmaske@gmail.com>
2026-04-23 16:10:37 +0200

    Create li element on submit

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const titleInput = document.getElementById("book-title");
 const authorInput = document.getElementById("book-author");
 const bookForm = document.getElementById("book-form");
+const bookList = document.getElementById("book-list");
 
 bookForm.addEventListener("submit", function (event) {
   event.preventDefault();
@@ -8,6 +9,7 @@ bookForm.addEventListener("submit", function (event) {
   const title = titleInput.value;
   const author = authorInput.value;
 
-  console.log(title);
-  console.log(author);
+  const newBook = document.createElement("li");
+  newBook.textContent = title + " " + author;
+  bookList.appendChild(newBook);
 });
