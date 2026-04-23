2d57993da5f6c10f0fab75c2420770ff1257b317
Vladan Masic <vladanmaske@gmail.com>
2026-04-23 16:46:15 +0200

    Add remove button

--- script.js
+++ script.js
@@ -6,17 +6,25 @@ const bookList = document.getElementById("book-list");
 bookForm.addEventListener("submit", function (event) {
   event.preventDefault();
 
-  const title = titleInput.value;
-  const author = authorInput.value;
+  const title = titleInput.value.trim();
+  const author = authorInput.value.trim();
 
   if (title === "" || author === "") {
     return;
   }
 
   const newBook = document.createElement("li");
+  const deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "Remove";
+
   newBook.textContent = title + " by " + author;
+  newBook.appendChild(deleteBtn);
   bookList.appendChild(newBook);
 
   titleInput.value = "";
   authorInput.value = "";
+
+  deleteBtn.addEventListener("click", function (event) {
+    newBook.removeChild();
+  });
 });
