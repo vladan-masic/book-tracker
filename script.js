83a808b759287e88af8f5ce2e0dfea8dfa2a1d62
Vladan Masic <vladanmaske@gmail.com>
2026-04-23 16:01:06 +0200

    Add input value handling for book title and author

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+const titleInput = document.getElementById("book-title");
+const authorInput = document.getElementById("book-author");
 const bookForm = document.getElementById("book-form");
 
 bookForm.addEventListener("submit", function (event) {
   event.preventDefault();
-  console.log("Form submitted");
+
+  const title = titleInput.value;
+  const author = authorInput.value;
+
+  console.log(title);
+  console.log(author);
 });
